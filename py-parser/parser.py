075e25bfa7e27474c19f3a5859a987dff479eab5
ApsilonXi <volkov.volkova2000@mail.ru>
2024-10-26 12:05:23 +0300

    0

--- py-parser/parser.py
+++ py-parser/parser.py
@@ -4,32 +4,26 @@ from selenium.webdriver.common.by import By
 import json
 import time
 
-# Настройка драйвера
-service = Service('c:\\Users\\volko\\Downloads\\edgedriver_win64\\msedgedriver.exe')  # Замените на путь к вашему ChromeDriver
-driver = webdriver.Edge(service=service)
+def parse(url):
 
-# URL товара
-url = 'https://www.wildberries.ru/catalog/242789818/detail.aspx'  # Замените на нужный URL товара
+    # Настройка драйвера
+    service = Service('c:\\Users\\volko\\Downloads\\edgedriver_win64\\msedgedriver.exe')  
+    driver = webdriver.Edge(service=service)
+    driver.get(url)
 
-# Открытие страницы товара
-driver.get(url)
+    time.sleep(3) 
+    # Извлечение данных о товаре
+    product = {}
 
-# Ждем загрузки страницы
-time.sleep(3)  # Увеличьте значение при необходимости
+    try:
+        product['name'] = driver.find_element(By.CLASS_NAME, 'product-page__title').text
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
-# Извлечение данных о товаре
-product = {}
+    driver.quit()
 
-try:
-    product['name'] = driver.find_element(By.CLASS_NAME, 'product-page__title').text
-except Exception as e:
-    print(f"Error occurred: {e}")
+    # Сохранение информации в JSON файл
+    with open('product.json', 'w', encoding='utf-8') as f:
+        json.dump(product, f, ensure_ascii=False, indent=4)
 
-# Закрытие драйвера
-driver.quit()
-
-# Сохранение информации в JSON файл
-with open('product.json', 'w', encoding='utf-8') as f:
-    json.dump(product, f, ensure_ascii=False, indent=4)
-
-print("Данные о товаре успешно сохранены в product.json")
+    print("Данные о товаре успешно сохранены в product.json")
